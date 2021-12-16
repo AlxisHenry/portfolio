50841428da2572a3090eb4e2849419e285800881
AlxisHenry <alexis.henry150357@gmail.com>
2021-12-16 15:33:48 +0100

    Change menu and css

--- DEVELOPPEMENT/~js/main.js
+++ DEVELOPPEMENT/~js/main.js
@@ -1,5 +1,6 @@
-    function BackHomeImage() {
-    // Change l'image du menu au clique droit sur celui-ci
+// Fonctions utilisées sur 'main.html'
+
+function BackHomeImage() {
     let chemin = "../../CHARTE GRAPHIQUE/ICONS"
 
     const IMGS = [ chemin + "/favicon-1.ico", chemin + "/favicon-2.ico", chemin + "/favicon-3.ico", chemin + "/favicon-4.ico"];
@@ -12,3 +13,8 @@
 
 }
 
+// Fonctions utilisées sur 'comprendre.html'
+
+// Fonctions utilisées sur 'presentation.html'
+
+// Fonctions utilisées sur 'projets.html'
