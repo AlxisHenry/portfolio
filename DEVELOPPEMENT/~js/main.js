1f34f4cb0dc842beea5100ef15764c8a93aa90cb
AlxisHenry <alexis.henry150357@gmail.com>
2021-12-21 15:02:57 +0100

    modif all

--- DEVELOPPEMENT/~js/main.js
+++ DEVELOPPEMENT/~js/main.js
@@ -1,6 +1,7 @@
 // Fonctions utilisées sur 'main.html'
 
 function BackHomeImage() {
+
     let chemin = "../../CHARTE GRAPHIQUE/ICONS"
 
     const IMGS = [ chemin + "/favicon-1.ico", chemin + "/favicon-2.ico", chemin + "/favicon-3.ico", chemin + "/favicon-4.ico"];
@@ -10,6 +11,13 @@ function BackHomeImage() {
     console.log(RandomIMGS);
 
     document.getElementById("img-retour-accueil").src = RandomIMGS;
+
+}
+
+function change() {
+
+    document.querySelector(".img1").classList.add('fas');
+    document.getElementById("img-centrale").src = "./@img/mpd-logo-300h.png";
 
 }
 
