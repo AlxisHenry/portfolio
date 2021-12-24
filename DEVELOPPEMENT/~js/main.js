0d77daf039d7fcea09ea4048585ac74e6f5ada41
AlxisHenry <alexis.henry150357@gmail.com>
2021-12-24 08:30:10 +0100

    config ok

--- DEVELOPPEMENT/~js/main.js
+++ DEVELOPPEMENT/~js/main.js
@@ -8,6 +8,7 @@ function change1() { // Image principale : projet Timken > Renvoi vers page proj
     document.querySelector(".img2").classList.remove('fas'); // Remove cercle plein de img2
     document.querySelector(".img3").classList.remove('fas'); // Remove cercle plein de img3
     document.querySelector(".img1").classList.add('fas'); // Add cercle plein à img1
+    document.getElementById("img-centrale").title = "TIMKEN"
     document.getElementById("img-centrale").src = "./DEVELOPPEMENT/@img/Timken.png"; // Modification de l'image
     document.getElementById("img-scnd").src = "./DEVELOPPEMENT/@img/Snipe-IT-logo.png"; // Modification de l'image
     document.getElementById("img-thrd").src = "./DEVELOPPEMENT/@img/téléchargement (1).png";
@@ -18,6 +19,7 @@ function change2() { // Image secondaire : portfolio > Renvoi vers page projet P
     document.querySelector(".img1").classList.add('far');
     document.querySelector(".img3").classList.remove('fas');
     document.querySelector(".img2").classList.add('fas');
+    document.getElementById("img-centrale").title = "SNIPE-IT"
     document.getElementById("img-centrale").src = "./DEVELOPPEMENT/@img/Snipe-IT-logo.png";
     document.getElementById("img-scnd").src = "./DEVELOPPEMENT/@img/Timken.png";
     document.getElementById("img-thrd").src = "./DEVELOPPEMENT/@img/téléchargement (1).png";
@@ -28,6 +30,7 @@ function change3() { // Troisième image 'No Projet Found' > Renvoi vers page de
     document.querySelector(".img1").classList.add('far');
     document.querySelector(".img2").classList.remove('fas');
     document.querySelector(".img3").classList.add('fas');
+    document.getElementById("img-centrale").title = "PROJECTS"
     document.getElementById("img-thrd").src = "./DEVELOPPEMENT/@img/Snipe-IT-logo.png";
     document.getElementById("img-centrale").src = "./DEVELOPPEMENT/@img/téléchargement (1).png";
     document.getElementById("href-img").href = "./DEVELOPPEMENT/~html/projets.html";
