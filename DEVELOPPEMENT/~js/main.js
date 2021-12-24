07717eb76f07f1ad3fd118f8a175c808f2167922
AlxisHenry <alexis.henry150357@gmail.com>
2021-12-24 12:21:57 +0100

    change interval finish

--- DEVELOPPEMENT/~js/main.js
+++ DEVELOPPEMENT/~js/main.js
@@ -14,7 +14,7 @@ function change1() { // Image principale : projet Timken > Renvoi vers page proj
     document.getElementById("img-thrd").src = "./DEVELOPPEMENT/@img/téléchargement (1).png";
     // document.getElementById("href-img").href = "./DEVELOPPEMENT/~html/projets-timken.html"; // Modification du href
     document.querySelector(".src").onclick = function () {
-        document.getElementById("href-img").href = "./DEVELOPPEMENT/~html/projets-timken.html"; // Modification du href
+        document.getElementById("href-img").href = "./DEVELOPPEMENT/~html/projet-timken.html"; // Modification du href
     }
 }
 function change2() { // Image secondaire : portfolio > Renvoi vers page projet Portfolio
@@ -28,7 +28,7 @@ function change2() { // Image secondaire : portfolio > Renvoi vers page projet P
     document.getElementById("img-thrd").src = "./DEVELOPPEMENT/@img/téléchargement (1).png";
     // document.getElementById("href-img").href = "./DEVELOPPEMENT/~html/projets-portfolio.html";
     document.querySelector(".src").onclick = function () {
-        document.getElementById("href-img").href = "./DEVELOPPEMENT/~html/projets-portfolio.html";
+        document.getElementById("href-img").href = "./DEVELOPPEMENT/~html/projet-portfolio.html";
     }
 }
 function change3() { // Troisième image 'No Projet Found' > Renvoi vers page de projets
@@ -43,16 +43,34 @@ function change3() { // Troisième image 'No Projet Found' > Renvoi vers page de
     document.querySelector(".img2").classList.remove('fas');
     document.querySelector(".img3").classList.add('fas');
     document.getElementById("img-centrale").title = "PROJECTS"
+    document.getElementById("img-scnd").src = "./DEVELOPPEMENT/@img/Timken.png";
     document.getElementById("img-thrd").src = "./DEVELOPPEMENT/@img/Snipe-IT-logo.png";
     document.getElementById("img-centrale").src = "./DEVELOPPEMENT/@img/téléchargement (1).png";
     // document.getElementById("href-img").href = "./DEVELOPPEMENT/~html/projets.html";
     document.querySelector(".src").onclick = function () {
-
-        
-
-        document.getElementById("href-img").href = "./DEVELOPPEMENT/~html/projets.html";
+        let path_table = ['/DEVELOPPEMENT/~html/projet-timken.html','./DEVELOPPEMENT/~html/projet-portfolio.html']
+        let random_path_selector = path_table[Math.floor(Math.random() * path_table.length)];
+        document.getElementById("href-img").href = random_path_selector;
     }
 }
+
+
+function change_interval() {
+
+    setTimeout("change1()",5000);
+    
+    setTimeout("change2()",15000);
+
+    setTimeout("change3()",25000);
+
+}
+
+setInterval("change_interval()", 25000)
+
+
+
+
+
 
 
 // Fonctions utilisées sur 'comprendre.html'
