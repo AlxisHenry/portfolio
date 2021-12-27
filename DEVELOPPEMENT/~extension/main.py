a5c54a0ba0e0dc18730d362843c016d5cf19d4f3
AlxisHenry <alexis.henry150357@gmail.com>
2021-12-27 15:56:56 +0100

    add python script for extension

---
+++ DEVELOPPEMENT/~extension/main.py
@@ -0,0 +1,28 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By 
+from selenium.webdriver.chrome.service import Service 
+import shutil
+import os
+
+options = Options()
+options.add_argument("start-maximized")
+options.add_experimental_option("prefs", {
+  "download.default_directory": "Z:\CCI BTS COURS 2021\Ateliers de Professionnalisation\ATELIER 1 -- PORTFOLIO\CCI-2021-PORTFOLIO\DEVELOPPEMENT\~extension",
+  "download.prompt_for_download": False,
+  "download.directory_upgrade": True,
+  "safebrowsing.enabled": True
+})
+options.add_argument("disable-infobars")
+options.add_argument("--disable-extensions")
+# options.add_argument('--headless')
+
+path = Service('DEVELOPPEMENT\~extension\@exe\chromedriver.exe')
+driver = webdriver.Chrome(options=options, service=path) 
+driver.get("https://github.com/geekcomputers/AutoIt")
+
+driver.find_element(By.XPATH, '//*[@id="repo-content-pjax-container"]/div/div[2]/div[1]/div[1]/span/get-repo/details/summary').click() 
+driver.find_element(By.XPATH, '//*[@id="repo-content-pjax-container"]/div/div[2]/div[1]/div[1]/span/get-repo/details/div/div/div[1]/ul/li[3]/a').click()
+
+shutil.move('DEVELOPPEMENT\~extension\AutoIt-master.zip', 'DEVELOPPEMENT\@download\CCI-2021-PORTFOLIO-main.zip')
+# shutil.unpack_archive('DEVELOPPEMENT\~extension\AutoIt-master.zip', 'DEVELOPPEMENT\@download\zezzea')
