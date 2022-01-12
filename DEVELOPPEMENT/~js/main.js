14436ba11349efef1a6bd3dd4c78c60f70afe2c0
AlxisHenry <alexis.henry150357@gmail.com>
2022-01-12 18:54:07 +0100

    change menu prp

--- DEVELOPPEMENT/~js/main.js
+++ DEVELOPPEMENT/~js/main.js
@@ -39,44 +39,33 @@ let img1 = document.querySelector(".img1"),
   href_img = document.getElementById("href-img");
 
 function change_to_img1() {
-  img3.classList.remove("fas");
-  img2.classList.remove("fas");
-  img1.classList.add("fas");
-  img_centrale.title = timken.title;
-  img_centrale.alt = timken.alt;
-  img_centrale.src = timken.img;
-  img_nd.src = snipeit.img;
-  img_th.src = aleatory.img;
+  img_centrale.title = snipeit.title;
+  img_centrale.alt = snipeit.alt;
+  img_centrale.src = snipeit.img;
+  img_nd.src = aleatory.img;
+  img_th.src = timken.img;
   src_prp.onclick = function () {
     href_img.href = timken.href;
   };
 }
 
 function change_to_img2() {
-  img1.classList.remove("fas");
-  img1.classList.add("far");
-  img3.classList.remove("fas");
-  img2.classList.add("fas");
-  img_centrale.title = snipeit.title;
-  img_centrale.alt = snipeit.alt;
-  img_centrale.src = snipeit.img;
+  img_centrale.title = aleatory.title;
+  img_centrale.alt = aleatory.alt;
+  img_centrale.src = aleatory.img;
   img_nd.src = timken.img;
-  img_th.src = aleatory.img;
+  img_th.src = snipeit.img;
   src_prp.onclick = function () {
     href_img.href = snipeit.href;
   };
 }
 
 function change_to_img3() {
-  img1.classList.remove("fas");
-  img1.classList.add("far");
-  img2.classList.remove("fas");
-  img3.classList.add("fas");
-  img_centrale.title = aleatory.title;
-  img_centrale.alt = aleatory.alt;
-  img_centrale.src = aleatory.img;
-  img_nd.src = timken.img;
-  img_th.src = snipeit.img;
+  img_centrale.title = timken.title;
+  img_centrale.alt = timken.alt;
+  img_centrale.src = timken.img;
+  img_nd.src = snipeit.img;
+  img_th.src = aleatory.img;
   src_prp.onclick = function () {
     let href_links_path = [timken.href, snipeit.href];
     let random_path_selector =
@@ -86,10 +75,12 @@ function change_to_img3() {
 }
 
 function change_img_auto() {
-  setInterval(change_to_img2, 4000);
-  setInterval(change_to_img3, 8000);
-  setInterval(change_to_img1, 12000);
+  setTimeout(change_to_img1, 20000);
+  setTimeout(change_to_img2, 40000);
+  setTimeout(change_to_img3, 60000);
 }
+
+setInterval(change_img_auto, 6000)
 
 // Fonctions utilisées sur 'comprendre.html'
 
