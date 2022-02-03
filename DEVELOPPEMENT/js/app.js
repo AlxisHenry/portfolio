2d44454098cb58539f7b3dd0cb61c7819c678693
AlxisHenry <alexis.henry150357@gmail.com>
2022-02-03 10:26:57 +0100

    🏌️‍♂️# Grid

--- DEVELOPPEMENT/js/app.js
+++ DEVELOPPEMENT/js/app.js
@@ -1,7 +1,7 @@
 const grid_Template = document.querySelector('.grid-projects');
 let id = 1;
 
-for (let div = 0; div < 9; div++){
+for (let div = 0; div < 12; div++){
    let div_toAdd = `<div class="projet-presentation-grid" id="pr${id}"></div>`;
    grid_Template.insertAdjacentHTML('beforeend', div_toAdd)
    id++
