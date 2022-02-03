c599b234ffb3f92679d56c727812b8f9979eaa17
AlxisHenry <alexis.henry150357@gmail.com>
2022-02-03 09:55:28 +0100

    🏌️‍♂️> Dev project page : try to make a grid

---
+++ DEVELOPPEMENT/js/app.js
@@ -0,0 +1,9 @@
+const grid_Template = document.querySelector('.grid-projects');
+let id = 1;
+
+for (let div = 0; div < 9; div++){
+   let div_toAdd = `<div class="projet-presentation-grid" id="pr${id}"></div>`;
+   grid_Template.insertAdjacentHTML('beforeend', div_toAdd)
+   id++
+}
+
