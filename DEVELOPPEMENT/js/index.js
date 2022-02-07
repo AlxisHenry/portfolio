bc522a3566041ec8075265bc0cacc8b44ebce072
AlxisHenry <alexis.henry150357@gmail.com>
2022-02-07 15:40:47 +0100

    🏌️‍♂️# Add module file for import BrowserName

--- DEVELOPPEMENT/js/index.js
+++ DEVELOPPEMENT/js/index.js
@@ -1,4 +1,5 @@
 import { images } from "./modules/data.js";
+import { browserName } from "./modules/browser.js";
 
 document.getElementById("src-img-centrale").insertAdjacentHTML(
   "afterbegin",
@@ -156,27 +157,6 @@ function LoadingFunction() {
 }
 
 // ------------------ FIREFOX & SAFARI (Using JS Platform librairies)  ------------------
-
-var browserName = (function (agent) {
-  switch (true) {
-    case agent.indexOf("edge") > -1:
-      return "MS Edge";
-    case agent.indexOf("edg/") > -1:
-      return "Edge ( chromium based)";
-    case agent.indexOf("opr") > -1 && !!window.opr:
-      return "Opera";
-    case agent.indexOf("chrome") > -1 && !!window.chrome:
-      return "Chrome";
-    case agent.indexOf("trident") > -1:
-      return "MS IE";
-    case agent.indexOf("firefox") > -1:
-      return "Firefox";
-    case agent.indexOf("safari") > -1:
-      return "Safari";
-    default:
-      return "other";
-  }
-})(window.navigator.userAgent.toLowerCase());
 
 if (browserName == "Chrome") {
   console.log("You use a good browser :)");
