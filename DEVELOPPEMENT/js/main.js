ba78625fc21463e997e64afd3fd78f949c9d55b4
AlxisHenry <alexis.henry150357@gmail.com>
2022-02-21 15:43:16 +0100

    🏌️‍♂️# ✅ Legal Notices

--- DEVELOPPEMENT/js/main.js
+++ DEVELOPPEMENT/js/main.js
@@ -5,13 +5,12 @@ import { images } from "./modules/data.js";
 // ------------------ HISTORY BACK ICON  ------------------
 
 document.getElementById("btn-rar").addEventListener("click", () => {
-    history.back();
+  history.back();
 });
 
 // ------------------ SEND TO DISCORD ICON ------------------
 
-document.getElementById("href-discord").addEventListener("click", () => {
-});
+document.getElementById("href-discord").addEventListener("click", () => {});
 
 // ------------------ SLEEP FUNC ------------------
 
@@ -24,13 +23,13 @@ export function sleepFor(sleepDuration) {
 
 // ------------------ LOADER ------------------
 
-if (document.querySelector('.loader-style-display')) {
-  const loader = document.querySelector('.loader-style-display');
-  const LoaderImg = `<img id="loader-aleatory-img" alt="${images.aleatory.alt}" title="aleatory" src="${images.aleatory.img}"/>`
-  loader.insertAdjacentHTML('afterbegin', LoaderImg);
-  
-  window.addEventListener('load', displayLoader);
-  
+if (document.querySelector(".loader-style-display")) {
+  const loader = document.querySelector(".loader-style-display");
+  const LoaderImg = `<img id="loader-aleatory-img" alt="${images.aleatory.alt}" title="aleatory" src="${images.aleatory.img}"/>`;
+  loader.insertAdjacentHTML("afterbegin", LoaderImg);
+
+  window.addEventListener("load", displayLoader);
+
   function displayLoader() {
     loader.className += " loader-none";
   }
@@ -58,7 +57,10 @@ const outTable = menu_deroulantTable.addEventListener("mouseout", () => {
 
 const date = new Date();
 
-if (document.querySelector("#index_page") || document.querySelector("#footer_page")){
+if (
+  document.querySelector("#index_page") ||
+  document.querySelector("#footer_page")
+) {
   var href_to_mentions = "./DEVELOPPEMENT/html/mentions-legales.html";
 } else {
   var href_to_mentions = "../html/inprogress.html";
@@ -70,20 +72,23 @@ All Rights Reserved</span></a>`;
 
 const addHTML_toFooter = document
   .getElementById("foot")
-  .insertAdjacentHTML("afterbegin", FooterText);  
+  .insertAdjacentHTML("afterbegin", FooterText);
 
 // ------------------ FAVICON ------------------
 
-if (document.querySelector("#index_page")){
+if (document.querySelector("#index_page")) {
   let href_to_favicon = "./DEVELOPPEMENT/assets/ico/favicon.png";
   const favicon = `<link rel="icon" href="${href_to_favicon}" />`;
   document.querySelector("title").insertAdjacentHTML("afterend", favicon);
 } else {
-  let href_to_favicon = "../assets/ico/favicon.png"; 
+  let href_to_favicon = "../assets/ico/favicon.png";
   const favicon = `<link rel="icon" href="${href_to_favicon}" />`;
   document.querySelector("title").insertAdjacentHTML("afterend", favicon);
 }
 
 // ------------------ DEBUG ------------------
 
-console.log(`%cIf you see a bug, please report them on : https://github.com/AlxisHenry/CCI-2021-PORTFOLIO`, "color: red; font-size:10px;");
+console.log(
+  `%cIf you see a bug, please report them on : https://github.com/AlxisHenry/CCI-2021-PORTFOLIO`,
+  "color: red; font-size:10px;"
+);
