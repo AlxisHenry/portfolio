d6eae6ac4d4bfa46ae70a15daf81a28dcd07a186
AlxisHenry <alexis.henry150357@gmail.com>
2022-04-19 11:12:02 +0200

    🏌 # Try to use fetch and remove jquery from project

--- src/js/veilles.js
+++ src/js/veilles.js
@@ -1,20 +1,36 @@
-$(".research-veille-technologique").keyup(function () {
-  var DynamicResearch = $(".research-veille-technologique").val();
-  var cards = $(".cards");
+document.querySelector(".research-veille-technologique").addEventListener('keyup', () => {
+  var DynamicResearch = document.querySelector(".research-veille-technologique").value;
+  var cards = document.querySelector(".cards");
   SendResearch(DynamicResearch, cards);
 });
 
-function SendResearch(value, dom) {
-  var param = "user_research=" + value;
+function SendResearch(value, element) {
+  console.log(value)
+  var search_value = "user_research=" + value;
   $.ajax({
     type: "GET",
     url: "../php/dynamic-search.php",
-    data: param,
+    data: search_value,
     success: function (search_result) {
-      dom.html(search_result).show();
+      element.innerHTML = '';
+      element.insertAdjacentHTML('beforeend', search_result);
     },
     error: function () {
-      console.log("Erreur");
     },
   });
 }
+
+/*
+function SendResearch(value, element) {
+  var search_value = "user_research=" + value;
+  fetch('../php/dynamic-search.php', {
+    method: 'POST',
+    header: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(search_value),
+  }).then((response) => response.json()
+  ).then((array) => {
+    console.log(array);
+  })
+}*/
