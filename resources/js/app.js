9d826f70c1799eb0f3c5c5d877fb5c5108dd33a1
AlxisHenry <alexis.henry150357@gmail.com>
2022-04-26 07:59:47 +0200

    Start menu burger

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,15 +3,26 @@ console.log('App js')
 
 function toggleNav() {
 
-const button = document.querySelector('.burger-button');
+const button = document.querySelector('.burger-button')
+const element = document.querySelector('.menu-navigation')
 
 if (button) {
         button.addEventListener('click', (e) => {
-            button.classList.toggle('is-active');
-            e.preventDefault();
-        }, false);
+            button.classList.toggle('is-active')
+            if (element){
+                if (!element.classList.contains('nav-up-animation')) {
+                    element.classList.remove('nav-down-animation')
+                    element.classList.add('nav-up-animation')
+                } else {
+                    element.classList.remove('nav-up-animation')
+                    element.classList.add('nav-down-animation')
+                }
+            }
+            e.preventDefault()
+
+        }, false)
     }
 }
 
-toggleNav();
+toggleNav()
 
--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-
+<head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
@@ -12,8 +12,18 @@
 </head>
 <body>
 
+
+
 <button class="burger-button" type="button" role="button" aria-label="open/close navigation"><i></i></button>
 
+<nav class="burger-element menu-navigation">
+
+    <div class="nav-content"><span class="nav-title">Qui suis-je ?</span></div>
+    <div class="nav-content"><span class="nav-title">Mes projets</span></div>
+    <div class="nav-content"><span class="nav-title">Mes projets2</span></div>
+    <div class="nav-content"><span class="nav-title">Mes projets3</span></div>
+
+</nav>
 
 
 </body>
