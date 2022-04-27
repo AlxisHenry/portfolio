ce91f5a3c07d5b97682faabffb6614b12d770923
AlxisHenry <alexis.henry150357@gmail.com>
2022-04-27 19:08:25 +0200

    Test run script to up test site

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -4,15 +4,13 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Portfolio</title>
+    <title>My Website Portfolio</title>
 
     <link rel="stylesheet" href="{{ url('css/app.css') }}">
     <link rel="icon" href="{{ url('assets/images/favicon.png') }}" />
 
 </head>
 <body>
-
-
 
 <button class="burger-button" type="button" role="button" aria-label="open/close navigation"><i></i></button>
 
