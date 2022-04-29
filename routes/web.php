09cf60276cc1cffb64b06fea38a7b50a3b319eb0
AlxisHenry <alexis.henry150357@gmail.com>
2022-04-29 21:03:55 +0200

    Add routes

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 //Route::get('', fn() => view('welcome'));
 
 Route::get('/', fn() => view('homepage'));
+
+Route::get('/welcome', fn() => view('welcome'));
+
