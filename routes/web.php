a9e1c273ebc4d8e38f6f09959eed496249514582
AlxisHenry <alexis.henry150357@gmail.com>
2022-05-02 22:34:13 +0200

    Change font / Route

--- routes/web.php
+++ routes/web.php
@@ -23,4 +23,4 @@ Route::view('/about', 'homepage', ['title' => 'About me - Henry Alexis', 'navbar
 
 Route::view('/projects', 'homepage', ['title' => 'Projects - Henry Alexis', 'navbar' => 'projects']);
 
-Route::view('/board', 'homepage', ['title' => 'Articles? - Henry Alexis', 'navbar' => 'board']);
+Route::view('/board', 'homepage', ['title' => 'Board - Henry Alexis', 'navbar' => 'board']);
