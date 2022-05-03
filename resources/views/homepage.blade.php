fc33cc723a93206f49aae008df9275181f5c5593
AlxisHenry <alexis.henry150357@gmail.com>
2022-05-03 11:43:31 +0200

    Homepage up

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -4,7 +4,8 @@
 <section class="__homepage__presentation__">
 
     <div class="screen">
-
+        <!--<img class="identity__screen" src="{{ url('/assets/images/scrzeen.jpg') }}">-->
+        <img class="identity__screen" style="border-radius: 50%;" src="https://cdn.discordapp.com/attachments/900446327333322782/970982374965903360/unknown.png">
     </div>
 
     <div class="__presentation__myself__">
@@ -19,8 +20,9 @@
 
         <div class="language">
             <img src="{{ url('assets/svg/javascript.svg') }}" alt="Javascript" title="Javascript" class="language-icon">
-            <img src="{{ url('assets/svg/nodejs.svg') }}" alt="NodeJS" title="NodeJS" class="language-icon">
             <img src="{{ url('assets/svg/php.svg') }}" alt="PHP" title="PHP" class="language-icon">
+            <img src="{{ url('assets/svg/python.svg') }}" alt="Python" title="Python" class="language-icon">
+            <img src="{{ url('assets/svg/nodejs.svg') }}" alt="NodeJS" title="NodeJS" class="language-icon">
             <img src="{{ url('assets/svg/laravel.svg') }}" alt="Laravel" title="Laravel" class="language-icon">
             <img src="{{ url('assets/svg/bash.svg') }}" alt="Bash" title="Bash" class="language-icon">
         </div>
