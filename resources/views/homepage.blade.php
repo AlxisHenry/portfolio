1d44a361f2df0fc3500facec6ff1ba3dc1433483
AlxisHenry <alexis.henry150357@gmail.com>
2022-05-05 18:05:46 +0200

    Home responsive & design / Menu Animation

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -16,10 +16,11 @@
                         </div>
                         <div class="__language__">
                             <img src="{{ url('assets/svg/javascript.svg') }}" alt="Javascript" title="Javascript" class="language_icon">
-                            <img src="{{ url('assets/svg/nodejs.svg') }}" alt="NodeJS" title="NodeJS" class="language_icon">
+                          <!--  <img src="" alt="NodeJS" title="NodeJS" class="language_icon"> -->
                             <img src="{{ url('assets/svg/php.svg') }}" alt="PHP" title="PHP" class="language_icon">
-                            <img src="{{ url('assets/svg/laravel.svg') }}" alt="Laravel" title="Laravel" class="language_icon">
                             <img src="{{ url('assets/svg/python.svg') }}" alt="Python" title="Python" class="language_icon">
+                            <img src="{{ url('assets/svg/laravel.svg') }}" alt="Laravel" title="Laravel" class="language_icon">
+                            <img src="{{ url('assets/svg/powershell.svg') }}" alt="Powershell" title="Powershell" class="language_icon">
                             <img src="{{ url('assets/svg/bash.svg') }}" alt="Bash" title="Bash" class="language_icon">
                         </div>
                     </div>
@@ -28,8 +29,6 @@
                     </div>
                 </div>
         </div>
-
-
 
 </section>
 
