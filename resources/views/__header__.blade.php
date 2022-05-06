33b28c0fda9bd5aa70cae5154d18eb26616e128a
AlxisHenry <alexis.henry150357@gmail.com>
2022-05-06 15:11:42 +0200

    ✨ Github icon on navbar

--- resources/views/__header__.blade.php
+++ resources/views/__header__.blade.php
@@ -6,5 +6,6 @@
     <title>{{ $title }}</title>
     <link rel="stylesheet" href="{{ url('css/app.css') }}">
     <link rel="icon" href="{{ url('assets/images/favicon.png') }}" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"/>
 </head>
 <body>
