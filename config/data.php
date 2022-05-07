a32df5935e58825dad1028e7c0150994e829422e
AlxisHenry <alexis.henry150357@gmail.com>
2022-05-07 13:07:44 +0200

    🎨 Homepage responsive / Design cards (use google translate php)

---
+++ config/data.php
@@ -0,0 +1,14 @@
+<?php
+
+
+
+return [
+
+    'name' => 'Henry Alexis',
+    'job' => 'Web Developer',
+
+    'presentation' => "Hi, I'm Henry Alexis, Web Developer since 2022, based in Strasbourg, France.",
+
+
+];
+
--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -1,5 +1,13 @@
 @include('__header__')
 
+<?php
+use Stichoza\GoogleTranslate\GoogleTranslate;
+$Google = new GoogleTranslate();
+$Google->setSource('fr');
+$Google->setTarget('en');
+?>
+
+
 @include('__navbar__')
 
 <section class="__homepage__presentation__">
@@ -8,10 +16,10 @@
                 <div class="__main__presentation__">
                     <div class="__me__">
                         <div class="name">
-                            Henry Alexis
+                            {{ config('data.name') }}
                         </div>
                         <div class="job">
-                            Web Developer
+                            {{ config('data.job') }}
                         </div>
                         <div class="__language__">
                             <img src="{{ url('assets/svg/javascript.svg') }}" alt="Javascript" title="Javascript" class="language_icon">
@@ -42,8 +50,12 @@
 
         <div class="__about__">
             <span class="_about_descript_">
-                Hi, I'm Henry Alexis,<br>
-                French Web Developer based in Strasbourg, France.
+
+                {{ config('data.presentation') }} <br>
+                Hope to become Full Stack Developer. <br>
+                I'm ready to learn every language if I need to use it. <br>
+                My favorites langague is Javascript. <br>
+
             </span>
 
             <div class="__more__about__">
@@ -55,6 +67,44 @@
         </div>
 
     </div>
+
+</section>
+
+<section id="__spoilerProjects" class="__spoiler__projects__">
+
+</section>
+
+<section id="__spoilerCards" class="__spoiler__cards__ hidden">
+
+    @foreach(DB::select('SELECT * FROM `Articles`
+                            INNER JOIN Dates ON Articles.identifier = Dates.identifier
+                            INNER JOIN Images ON Articles.identifier = Images.identifier
+                            INNER JOIN Themes ON Articles.identifier = Themes.identifier
+                            WHERE Articles.identifier > 155 AND Articles.identifier < 161') as $card)
+
+        <div class="__article__card__ __article__nb__{{ $card->identifier }}__">
+
+            <div class="__article__image__">
+                <img src="{{ $card->LinkImage }}" alt="{{ $Google->translate(str($card->introduction)) }}" title="{{ $Google->translate($card->title) }}">
+            </div>
+
+            <div class="__article__date__">
+                <time data-time="{{ date($card->UploadDate) }}"> Published on {{ date('d/m/Y', strtotime(date($card->UploadDate)))  }}</time>
+            </div>
+
+            <div class="__article__title__">
+                <span>{{ $Google->translate($card->title) }}</span>
+            </div>
+
+            <div class="__article_url__">
+                <a href="{{ $card->UrlArticle }}" target="_blank">
+                    <button>{{ $Google->translate("Voir l'article") }}</button>
+                </a>
+            </div>
+
+        </div>
+
+    @endforeach
 
 </section>
 
