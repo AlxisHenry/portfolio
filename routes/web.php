28e227242b274280148dd8d6e14f036f00aedbd9
AlxisHenry <alexis.henry150357@gmail.com>
2022-05-10 07:57:38 +0200

    New route with parameter

--- routes/web.php
+++ routes/web.php
@@ -5,15 +5,23 @@ use Illuminate\Support\Facades\Route;
 /* RouteServiceProvider */
 
 Route::view('/home',   'homepage', ['title' => 'Home - Henry Alexis',
-                                             'navbar' => 'home']);
+                                             'navbar' => 'home',
+                                             'og_description' => 'Portfolio Henry Alexis - Homepage']);
 
 Route::view('/projects', 'projects', ['title' => 'Projects - Henry Alexis',
-                                                'navbar' => 'projects']);
+                                                'navbar' => 'projects',
+                                                'og_description' => 'Portfolio Henry Alexis - Projects']);
 
 Route::view('/board', 'board', ['title' => 'Board - Henry Alexis',
-                                            'navbar' => 'board']);
+                                            'navbar' => 'board',
+                                            'og_description' => 'Portfolio Henry Alexis - Board Veille Technologique']);
+
+Route::get('board/article/{title}', function ($title = 'null') {
+    return view('board_article');
+})->name('article');
 
 Route::view('/about', 'about', ['title' => 'About me - Henry Alexis',
-                                            'navbar' => 'about']);
+                                            'navbar' => 'about',
+                                            'og_description' => 'Portfolio Henry Alexis - About me']);
 
 Route::redirect('/', 'home');
