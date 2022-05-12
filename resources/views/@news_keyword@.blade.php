9fcae6ee7bec14e080c0fe00d637edac83b4e14f
AlxisHenry <alexis.henry150357@gmail.com>
2022-05-12 08:16:40 +0200

    Template cards

--- resources/views/@news_keyword@.blade.php
+++ resources/views/@news_keyword@.blade.php
@@ -9,29 +9,9 @@
 
         @foreach($CORRESPONDING_KEYWORD_ARTICLE as $card)
 
-                <div class="__article__card__ __article__nb__{{ $card->identifier }}__ ">
+            @include('__card__')
 
-                    <div class="__article__image__">
-                        <img src="{{ $card->LinkImage }}" alt="{{ $Google->translate(str($card->AltImage)) }}" title="{{ $Google->translate($card->title) }}">
-                    </div>
-
-                    <div class="__article__date__">
-                        <time data-time="{{ date($card->UploadDate) }}"> Published on {{ date('d/m/Y', strtotime(date($card->UploadDate)))  }}</time>
-                    </div>
-
-                    <div class="__article__title__">
-                        <span>{{ $Google->translate($card->title) }}</span>
-                    </div>
-
-                    <div class="__article_url__">
-                        <a href="/news/{{ substr($card->UrlArticle, strrpos($card->UrlArticle, '/', '0') + 1) }}">
-                            <button>{{ $Google->translate("Voir l'article") }}</button>
-                        </a>
-                    </div>
-
-                </div>
-
-            @endforeach
+        @endforeach
 
     </div>
 
--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -102,27 +102,7 @@
 
         @foreach($spoiler_cards as $card)
 
-            <div class="__article__card__ __article__nb__{{ $card->identifier }}__ ">
-
-                <div class="__article__image__">
-                    <img src="{{ $card->LinkImage }}" alt="{{ $Google->translate(str($card->AltImage)) }}" title="{{ $Google->translate($card->title) }}">
-                </div>
-
-                <div class="__article__date__">
-                    <time data-time="{{ date($card->UploadDate) }}"> Published on {{ date('d/m/Y', strtotime(date($card->UploadDate)))  }}</time>
-                </div>
-
-                <div class="__article__title__">
-                    <span>{{ $Google->translate($card->title) }}</span>
-                </div>
-
-                <div class="__article_url__">
-                    <a href="/news/{{ substr($card->UrlArticle, strrpos($card->UrlArticle, '/', '0') + 1) }}">
-                        <button>{{ $Google->translate("Voir l'article") }}</button>
-                    </a>
-                </div>
-
-            </div>
+            @include('__card__')
 
         @endforeach
 
