1f937554a5bc68ed999f8e36f8813fed2afa310a
AlxisHenry <alexis.henry150357@gmail.com>
2022-06-02 14:29:53 +0200

    Add fade-out on loader

--- resources/js/components/loader.js
+++ resources/js/components/loader.js
@@ -96,6 +96,9 @@ export const RemoveLoader = () => {
     // Hide loader
     const loader = document.querySelector('.loader')
     const body = document.querySelector('body')
-    loader.style.display = 'none'
+    loader.classList.add('disabled')
+    setTimeout(() => {
+        loader.style.display = 'none'
+    }, 375)
     body.classList.remove('loader-body')
 }
