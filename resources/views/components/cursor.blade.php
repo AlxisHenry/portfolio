953369edc6cd83cb60646c3b2b3369cc111f7caa
AlxisHenry <alexis.henry150357@gmail.com>
2022-06-06 11:33:24 +0200

    Refactor components (style/template)

--- resources/views/components/cursor.blade.php
+++ resources/views/components/cursor.blade.php
@@ -1,17 +1,1 @@
 <span class="cursor hidden"></span>
-
-<style>
-    .cursor {
-        position: fixed;
-        display: block;
-        top: 0;
-        left: 0;
-        width: 7px;
-        height: 7px;
-        background-color: yellow;
-        transition: all 100ms ease;
-        border-radius: 50%;
-        mix-blend-mode: difference;
-        z-index: 99;
-    }
-</style>
--- resources/views/components/scrollbar.blade.php
+++ resources/views/components/scrollbar.blade.php
@@ -1,25 +1,3 @@
-<div class="__loading__bar__">
-    <div class="__state__indicator__"></div>
+<div class="__scrollbar__">
+    <div class="__scrollbar__indicator__"></div>
 </div>
-
-<style>
-    .__loading__bar__ {
-        position: fixed;
-        top: 0;
-        left: 0;
-        height: 100%;
-        background-color: #242B40;
-        width: 3px;
-        z-index: 120;
-
-    }
-    .__loading__bar__ .__state__indicator__ {
-        animation: All ease-in 300ms;
-        position: relative;
-        height: 0;
-        top: 0;
-        left: 0;
-        width: 3px;
-        background-color: #52B3D9;
-    }
-</style>
