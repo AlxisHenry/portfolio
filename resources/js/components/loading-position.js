e31b0982a2cbcceac34ee1834c9815d6f7144e6d
AlxisHenry <alexis.henry150357@gmail.com>
2022-06-06 11:56:26 +0200

    Refactor components (style/template)

--- resources/js/components/loading-position.js
+++ resources/js/components/loading-position.js
@@ -1,6 +1,6 @@
 const LoadingPosition = (e) => {
     // Load scrollbar on the left of the page
-    const LoadingIndicator = document.querySelector('.__state__indicator__')
+    const LoadingIndicator = document.querySelector('.__scrollbar__indicator__')
     const TotalHeight = document.body.offsetHeight
     const PassedHeight = Math.round(window.scrollY + window.innerHeight)
     const LoadingState = Math.round((PassedHeight / TotalHeight) * 100)
