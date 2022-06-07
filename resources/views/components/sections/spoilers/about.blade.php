f7cfe542926e86d53a3a40dc30ba85b61a922ebd
AlxisHenry <alexis.henry150357@gmail.com>
2022-06-07 06:37:09 +0200

    Update about me spoiler

--- resources/views/components/sections/spoilers/about.blade.php
+++ resources/views/components/sections/spoilers/about.blade.php
@@ -15,9 +15,7 @@
                     Hi, I'm Henry Alexis, Web Developer Junior since <span class="years">2022</span>, based in Strasbourg, France.
                     I'm currently student at the <a data-aos="zoom-in" href="https://www.ccicampus.fr/" rel="noreferrer nofollow">CCI Campus</a> of Strasbourg.
                     My work at the <a data-aos="zoom-in" href="https://www.timken.com/fr/" rel="noreferrer nofollow">Timken Company</a> allows me to further my
-                    knowledge in development.
-
-                    My goal is to make my passion my job.
+                    knowledge in development.<span>&nbsp;My goal is to make my passion my job.</span>
 
                 <comment style="font-size: 22px;">&lt;/&gt;</comment>
 
