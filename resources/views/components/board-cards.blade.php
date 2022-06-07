16ff5e031d1f9802476ff671a0d25f72bf66d2dc
AlxisHenry <alexis.henry150357@gmail.com>
2022-06-07 06:36:18 +0200

    Add data information on board cards

--- resources/views/components/board-cards.blade.php
+++ resources/views/components/board-cards.blade.php
@@ -1,21 +1,21 @@
 <div class="__documentation__card__">
 
-    <div class="__documentation__card__title__">
+    <div class="__documentation__card__title__" data-title="{{ $Board->title }}" title="Title">
         <span class="title">{{ $Board->title }}</span>
     </div>
 
-    <div class="__documentation__card__description__">
-        <div class="description">
+    <div class="__documentation__card__description__" title="Description">
+        <div class="description" data-description="{{ $Board->description }}">
             {{ $Board->description }}
         </div>
     </div>
 
     <div class="__documentation__card__author__">
-        Published at <time data-time="{{date('d/m/Y',strtotime($Board->published_at))}}" data-edit="{{$Board->edit_at}}" data-published="{{$Board->published_at}}">
-            &nbsp; {{date('d/m/Y',strtotime($Board->published_at))}}</time>, by <span class="author">&nbsp;{{ $Board->author }}</span>.
+        Published at <time data-time="{{date('d/m/Y',strtotime($Board->published_at))}}" data-edit="{{$Board->edit_at}}" data-published="{{$Board->published_at}}" title="Published at">
+            &nbsp; {{date('d/m/Y',strtotime($Board->published_at))}}</time>, by <span class="author" data-author="{{ $Board->author }}" title="Author">&nbsp;{{ $Board->author }}</span>.
     </div>
 
-    <div class="__documentation__card__download__">
+    <div class="__documentation__card__download__" data-documentation="{{ $Board->documentationLink }}" title="Download this documentation">
         <a href="{{ $Board->documentationLink }}">
             <span> <i class="fa-solid fa-arrow-down-long"></i> Download</span>
             <div class="__documentation__card__download__animation__">
