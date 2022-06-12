c8b420830fa94aca1b57adb851fb05210869557a
AlxisHenry <alexis.henry150357@gmail.com>
2022-06-12 22:28:43 +0200

    Add contacts solution to about-me / Change the description

--- resources/views/components/sections/spoilers/about.blade.php
+++ resources/views/components/sections/spoilers/about.blade.php
@@ -8,18 +8,58 @@
 
         <div class="__about__">
 
-            <span class="_about_descript_">
+            <div class="_about_descript_">
 
-                <comment style="font-size: 22px;">&lt;/&gt;</comment>
+                <comment style="font-size: 16px;">&lt;/&gt;</comment>
 
-                    Hi, I'm Henry Alexis, Web Developer Junior since <span class="years">2022</span>, based in Strasbourg, France.
-                    I'm currently student at the <a data-aos="zoom-in" href="https://www.ccicampus.fr/" rel="noreferrer nofollow">CCI Campus</a> of Strasbourg.
-                    My work at the <a data-aos="zoom-in" href="https://www.timken.com/fr/" rel="noreferrer nofollow">Timken Company</a> allows me to further my
-                    knowledge in development.<span>&nbsp;My goal is to make my passion my job.</span>
+                    Hi, I'm Henry Alexis, junior web developer since <span class="years">2022</span>, based in Strasbourg, France <img alt="" src="https://cdn-icons-png.flaticon.com/512/197/197560.png" width="20">
+                    I'm currently attending a 2-year high  vocational sandwich course at the <a data-aos="zoom-in" href="https://www.ccicampus.fr/" rel="noreferrer nofollow">CCI Campus</a> of Strasbourg vocational sandwich course to become a web developer.
+                    Thanks to this internship I feel I have developed some <a href="/about">skills</a>. After my diploma, I will continue my studies with a license Application Developer Designer.
+                    Therefore I'm looking for a sandwich course starting in 2023. My dream is to make my passion my job.
 
-                <comment style="font-size: 22px;">&lt;/&gt;</comment>
+                <comment style="font-size: 16px;">&lt;/&gt;</comment>
 
-            </span>
+            </div>
+
+            <div class="_about_contact">
+
+                <div>
+                    <div>
+                        <a href="https://www.linkedin.com/in/alexishenry03/" rel="noreferrer nofollow">
+                            <img class="test" src="{{ url('assets/svg/contacts/linkedin.svg') }}" alt="Linkedin" title="Linkedin" width="24">
+                        </a>
+                        <a href="https://www.linkedin.com/in/alexishenry03/" rel="noreferrer nofollow">
+                            <span>alexishenry03</span>
+                        </a>
+                    </div>
+                    <div>
+                        <a class="to-contact-form">
+                            <img class="test" src="{{ url('assets/svg/contacts/mail.svg') }}" alt="Mail" title="Mail" width="20">
+                        </a>
+                        <a class="to-contact-form">
+                            <span>alexis.henry150357@gmail.com</span>
+                        </a>
+                    </div>
+                    <div>
+                        <a href="https://github.com/AlxisHenry03" rel="noreferrer nofollow">
+                            <img class="test" src="{{ url('assets/svg/contacts/github-blue.svg') }}" alt="Github" title="Github" width="24">
+                        </a>
+                        <a href="https://github.com/AlxisHenry" rel="noreferrer nofollow">
+                            <span>@AlxisHenry</span>
+                        </a>
+                    </div>
+                    <div>
+                        <a href="https://wakatime.com/@AlxisHenry" rel="noreferrer nofollow">
+                            <img class="test" src="{{ url('assets/svg/contacts/wakatime.svg') }}" alt="Wakatime" title="Wakatime" width="20">
+                        </a>
+                        <a href="https://wakatime.com/@AlxisHenry" rel="noreferrer nofollow">
+                            <span>@AlxisHenry</span>
+                        </a>
+                    </div>
+              </div>
+
+            </div>
+
 
         </div>
 
