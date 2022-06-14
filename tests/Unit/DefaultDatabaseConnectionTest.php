af1fe511b46cbe1a6c801bf3a95d4ad4dcbf65b4
AlxisHenry <alexis.henry150357@gmail.com>
2022-06-14 10:58:46 +0200

    Add unit test for database connexion

---
+++ tests/Unit/DefaultDatabaseConnectionTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Models\User;
+use Tests\TestCase;
+
+class DefaultDatabaseConnectionTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function test_environment_database_connection_is_mysql()
+    {
+        $user = User::where('user_id', 1)->first ();
+        $this->assertSame('admin', $user->username);
+    }
+}
