4a889dc0b30fa88b0d94ba49391ca721bb27b228
AlxisHenry <alexis.henry150357@gmail.com>
2022-06-14 11:07:34 +0200

    unit test for send emails

---
+++ tests/Feature/SendMailTest.php
@@ -0,0 +1,17 @@
+<?php
+
+
+use PHPUnit\Framework\TestCase;
+
+class SendMailTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function test_send_mail()
+    {
+        $this->assertTrue(true);
+    }
+}
