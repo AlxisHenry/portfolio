ae19980566c4094cc225613e25bf0f685cf0fad7
AlxisHenry <alexis.henry150357@gmail.com>
2022-06-15 22:08:49 +0200

    Update news page (add searchbar)

--- resources/js/templates/news.js
+++ resources/js/templates/news.js
@@ -1,5 +1,32 @@
 import {InputAnimation} from "../components/about";
 
+const RedirectToKeyword = () => {
+
+    let submitSearch = document.querySelector('.__submit__search__')
+    let submitValue = document.querySelector('.__search__input__')
+
+    submitValue.addEventListener('keyup', (e) => {
+        if (e.keyCode === 13) {
+            if (e.target.value.length > 3) {
+                submitSearch.click()
+            }
+        } else {
+            e.preventDefault()
+        }
+    })
+
+    submitSearch.addEventListener('click', (e) => {
+        let value = submitValue.value
+        if (value.length > 3) {
+            document.location.href = `/news/word/${value.toLowerCase()}`
+        } else {
+            e.preventDefault()
+        }
+    })
+
+}
+
 window.addEventListener('load', (e) => {
     InputAnimation(e)
+    RedirectToKeyword(e)
 })
--- resources/views/templates/news.blade.php
+++ resources/views/templates/news.blade.php
@@ -4,7 +4,26 @@
 
 <section class="__news__ test layout-maxed">
 
-    <div class="__cards__categories__">
+    @component('components.underline-title')
+        @slot('Title')
+            News
+        @endslot
+    @endcomponent
+
+    <div class="section-explication">
+        On this page are available a number of articles retrieved from France Inter.
+        I retrieved them using <a href="https://selenium-python.readthedocs.io/">selenium</a>, a <a href="https://www.python.org/">python</a> tool.
+        The source code is available on <a href="https://github.com/AlxisHenry/CCI-2021-PORTFOLIO/tree/v2.1.0/extension">github</a>.
+    </div>
+
+    <div class="__search__article__">
+        <label>
+            <input type="text" class="__search__input__" placeholder="Search">
+        </label>
+        <div class="__submit__search__"><i class="fa-solid fa-magnifying-glass"></i></div>
+    </div>
+
+    <div class="__news__cards__">
 
         @foreach($news as $card)
 
