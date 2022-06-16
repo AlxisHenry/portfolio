788e027c7364c1d9a98d71ed38efe943a2b249d7
AlxisHenry <alexis.henry150357@gmail.com>
2022-06-16 15:18:12 +0200

    Compteur de nombre de caractère #33

--- resources/js/components/contact-form.js
+++ resources/js/components/contact-form.js
@@ -1,4 +1,5 @@
 const CountContactAreaLength = () => {
+    // todo => Update animation / Block the user if he dom element
     const textArea = document.querySelector('.area-text-form')
     const keyCount = document.querySelector(".container-length-indicator .length-indicator .indicator .on")
     const limit = document.querySelector(".container-length-indicator .length-indicator .indicator .limit")
