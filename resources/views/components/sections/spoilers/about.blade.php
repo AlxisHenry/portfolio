cfad799e047471d8b996c94ea1e12d8840106f14
AlxisHenry <alexis.henry150357@gmail.com>
2022-06-30 11:51:23 +0200

    Change about-me description

--- resources/views/components/sections/spoilers/about.blade.php
+++ resources/views/components/sections/spoilers/about.blade.php
@@ -13,7 +13,7 @@
                 <comment style="font-size: 16px;">&lt;/&gt;</comment>
 
                     Hi, I'm Henry Alexis, junior web developer since <span class="years">2022</span>, based in Strasbourg, France <img alt="" src="https://cdn-icons-png.flaticon.com/512/197/197560.png" width="20">
-                    I'm currently attending a 2-year high  vocational sandwich course at the <a data-aos="zoom-in" href="https://www.ccicampus.fr/" rel="noreferrer nofollow">CCI Campus</a> of Strasbourg to become a web developer.
+                    I'm currently attending a 2-year work study program at <a data-aos="zoom-in" href="https://www.ccicampus.fr/" rel="noreferrer nofollow">CCI Campus</a> in Strasbourg to become a web developer.
                     Thanks to this internship I feel I have developed some <a href="/about">skills</a>. After my diploma, I will continue my studies with a license Application Developer Designer.
                     Therefore I'm looking for a sandwich course starting in 2023. My dream is to make my passion my job.
 
