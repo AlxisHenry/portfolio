3ca68966eb609959eec88bfbbac4c30d8a85d9ca
AlxisHenry <alexis.henry150357@gmail.com>
2022-07-01 09:17:05 +0200

    use Illuminate\Http\Request for "$request->input('name')"

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -31,12 +31,12 @@ class AdminController extends Controller
                 ]);
     }
 
-    public function View(string $view)
+    public function View(string $view, Request $request)
     {
 
         if (isset($_POST['username']) && isset($_POST['password'])) {
-            $username = $_POST['username'];
-            $password = $_POST['password'];
+            $username = $request->input('username');
+            $password = $request->input('password');
             Session::put('username', $username);
             Session::put('password', $password);
         } else {
@@ -85,7 +85,7 @@ class AdminController extends Controller
 
     }
 
-    public function Action(string $view, int $id, string $action)
+    public function Action(string $view, int $id, string $action, Request $request)
     {
         $target = null;
         $delete_status = false;
@@ -117,34 +117,34 @@ class AdminController extends Controller
             if(isset($_POST['type_action']) && isset($_POST['type_object'])) {
 
                 if(isset($_POST['identifier'])) {
-                    $type_object = $_POST['type_object'];
+                    $type_object = $request->input('type_object');
                     $data_obj = null;
                     switch ($type_object) {
                         case 'news':
                             $data_obj = News::find($id);
-                            $data_obj->introduction = $_POST['introduction'];
-                            $data_obj->UrlArticle = $_POST['link_news'];
-                            $data_obj->LinkImage = $_POST['link_img'];
-                            $data_obj->Theme = $_POST['themes'];
-                            $data_obj->ThemePrincipal = $_POST['theme_principal'];
+                            $data_obj->introduction = $request->input('introduction');
+                            $data_obj->UrlArticle = $request->input('link_news');
+                            $data_obj->LinkImage = $request->input('link_img');
+                            $data_obj->Theme = $request->input('themes');
+                            $data_obj->ThemePrincipal = $request->input('theme_principal');
                             break;
                         case 'projects':
                             $data_obj = Projects::find($id);
-                            $data_obj->description = $_POST['description'];
-                            $data_obj->GithubLink = $_POST['github_link'];
-                            $data_obj->languages = $_POST['languages'];
-                            $data_obj->linkImage = $_POST['link_img'];
-                            $data_obj->url_name = $_POST['url_name'];
+                            $data_obj->description = $request->input('description');
+                            $data_obj->GithubLink = $request->input('github_link');
+                            $data_obj->languages = $request->input('languages');
+                            $data_obj->linkImage = $request->input('link_img');
+                            $data_obj->url_name = $request->input('url_name');
                             break;
                         case 'resources':
                             $data_obj = Board::find($id);
-                            $data_obj->documentationLink = $_POST['documentation'];
-                            $data_obj->description = $_POST['description'];
+                            $data_obj->documentationLink = $request->input('documentation');
+                            $data_obj->description = $request->input('description');
                             break;
                     }
 
-                    $data_obj->title = $_POST['title'];
-                    $data_obj->author = $_POST['author'];
+                    $data_obj->title = $request->input('title');
+                    $data_obj->author = $request->input('author');
                     $data_obj->save();
                     $callback_message_status = true;
 
@@ -167,28 +167,27 @@ class AdminController extends Controller
             ]);
     }
 
-    public function New(string $view) {
+    public function New(string $view, Request $request) {
 
         $edited_status = false;
         $callback_message_status = false;
-
-        if (isset($_POST['type_object']) && isset($_POST['type_action'])) {
-            $type_object = $_POST['type_object'];
-            $type_action = $_POST['type_action'];
+        if ($request->input('type_object') && $request->input('type_action')) {
+            $type_object = $request->input('type_object');
+            $type_action = $request->input('type_action');
 
             if ($type_action === 'new') {
 
-                $title = $_POST['title'];
-                $author = $_POST['author'];
-                $published_at = $_POST['published_at'];
+                $title = $request->input('title');
+                $author = $request->input('author');
+                $published_at = $request->input('published_at');
 
                 switch ($type_object) {
                     case 'news':
-                        $link_news = $_POST['link_news'];
-                        $link_img = $_POST['link_img'];
-                        $themes = $_POST['themes'];
-                            $theme_principal = $_POST['theme_principal'];
-                            $introduction = $_POST['introduction'];
+                        $link_news = $request->input('link_news');
+                        $link_img = $request->input('link_img');
+                        $themes = $request->input('themes');
+                            $theme_principal = $request->input('theme_principal');
+                            $introduction = $request->input('introduction');
 
                             News::create([
                             'title' => $title,
@@ -203,8 +202,8 @@ class AdminController extends Controller
 
                         break;
                     case 'resources':
-                        $documentation = $_POST['documentation'];
-                        $description = $_POST['description'];
+                        $documentation = $request->input('documentation');
+                        $description = $request->input('description');
 
                         Board::create([
                             'title' => $title,
@@ -216,11 +215,11 @@ class AdminController extends Controller
                         ]);
                         break;
                     case 'projects':
-                        $description = $_POST['description'];
-                        $url_name = $_POST['url_name'];
-                        $github_link = $_POST['github_link'];
-                        $link_img = $_POST['link_img'];
-                        $languages = $_POST['languages'];
+                        $description = $request->input('description');
+                        $url_name = $request->input('url_name');
+                        $github_link = $request->input('github_link');
+                        $link_img = $request->input('link_img');
+                        $languages = $request->input('languages');
 
                         Projects::create([
                             'title' => $title,
