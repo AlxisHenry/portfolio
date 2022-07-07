df1011731756c073c0476c8f72b56081e00301d8
AlxisHenry <alexis.henry150357@gmail.com>
2022-07-07 18:21:32 +0200

    Add a test for emails

--- tests/Feature/SendEmailTest.php
+++ tests/Feature/SendEmailTest.php
@@ -2,9 +2,7 @@
 
 namespace Tests\Feature;
 
-use App\Mail\Contact;
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
+use App\Mail\ContactMailable;
 use Illuminate\Support\Facades\Mail;
 use Tests\TestCase;
 
@@ -17,7 +15,21 @@ class SendEmailTest extends TestCase
      */
     public function test_send_email()
     {
-        $email = "alexishenry.dev@gmail.com";
-        Mail::to($email)->send(new Contact());
+        $owner_email = "alexis.henry150357@gmail.com";
+
+        $request = [
+            'name' => 'Alexis',
+            'email' => 'alexis150357@gmail.com',
+            'object' => 'Object',
+            'content' => 'Contenu Test',
+        ];
+
+        Mail::fake();
+
+        Mail::to($owner_email)->queue(new ContactMailable($request));
+
+        Mail::assertQueued(ContactMailable::class);
+
     }
+
 }
