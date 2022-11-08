a1a815eea01b60ff3783d2d7ff2cc78a5b95da0e
AlexisHenry <alexis.henry150357@gmail.com>
2022-11-08 08:04:56 +0100

    use url to link matomo

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -34,6 +34,6 @@
     <link rel="manifest" href="{{ url('assets/icons/site.webmanifest') }}">
     <meta name="msapplication-TileColor" content="#da532c">
     <meta name="theme-color" content="#ffffff">
-    <script src="{{ url('js/matomo.js') }}"></script>
+    <script src="https://analytics.alexishenry.eu/sync.js"></script>
 </head>
 
