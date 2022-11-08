4c5806051e7402db73d5f1f8b8013bcd6f485469
AlxisHenry <alexis.henry150357@gmail.com>
2022-11-08 07:07:39 +0100

    Optimize

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,18 +1,12 @@
 @include('layouts.header')
-
 @if(Route::currentRouteName() === "home" || Route::currentRouteName() === 'home.contact')
-    <body class="loader-body">
-    @include('layouts.loader', ['status' => true])
+        <body class="loader-body">
+        @include('layouts.loader', ['status' => true])
     @else
         <body>
 @endif
-
 @include('components.scrollbar')
 @include('components.cursor')
-
 @include('layouts.navbar')
-
-    @yield('content')
-
+@yield('content')
 @include('layouts.footer')
-
