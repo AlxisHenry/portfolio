7188a0e93e823ef1936a5b7999bbeef24fbb17b5
AlxisHenry <alexis.henry150357@gmail.com>
2022-11-18 19:39:25 +0100

    database import script

--- database/imports/import.sh
+++ database/imports/import.sh
@@ -1,25 +1,3 @@
-echo "\n";
-echo "\e[1;42m                                   \e[0m";
-echo "\e[1;42m    Configure default database     \e[0m";
-echo "\e[1;42m                                   \e[0m";
-echo "\n";
-echo "\e[41m                                               \e[0m";
-echo "\e[41m    Your database name need to be : main  !    \e[0m";
-echo "\e[41m                                               \e[0m";
-
-sleep 2
-
-echo "\n";
-echo "\e[1;42m                                \e[0m";
-echo "\e[1;42m      Starting import data      \e[0m";
-echo "\e[1;42m                                \e[0m";
-
-sudo mariadb -e "source /var/www/main/database/imports/news.sql"
-sudo mariadb -e "source /var/www/main/database/imports/resources.sql"
-sudo mariadb -e "source /var/www/main/database/imports/projects.sql"
-
-echo "\n";
-echo "\e[1;42m                           \e[0m";
-echo "\e[1;42m      Import finished      \e[0m";
-echo "\e[1;42m                           \e[0m";
-echo "\n";
+sudo mariadb -e "source news.sql"
+sudo mariadb -e "source resources.sql"
+sudo mariadb -e "source projects.sql"
