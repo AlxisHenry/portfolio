6ea132896a21fcffd31c8528ca63ba97e55d2e16
AlxisHenry <alexis.henry150357@gmail.com>
2022-11-21 10:31:25 +0100

    Fix #35 : remove skills aos animations

--- resources/views/components/skills.blade.php
+++ resources/views/components/skills.blade.php
@@ -1,10 +1,5 @@
-<div class="skill" data-aos="slide-up" data-aos-duration="{{ $animateTiming }}">
-
+<div class="skill" >
     <div class="skill-part">
-
         <img title="{{ucfirst($skill)}}" src="{{ asset("assets/svg/skills/$svg.svg") }}" alt="Présentation de mes compétences : {{$skill}}">
-
     </div>
-
-
 </div>
