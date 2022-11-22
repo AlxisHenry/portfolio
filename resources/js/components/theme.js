74be354e699586397a0b8d21cfac57344b1777b1
AlxisHenry <alexis.henry150357@gmail.com>
2022-11-22 08:55:32 +0100

    Format theme functions

--- resources/js/components/theme.js
+++ resources/js/components/theme.js
@@ -1,24 +1,22 @@
 const switcher = document.querySelector(".__theme__main__");
 const indicator = switcher.children[0];
-const themes = {
-  light: "light",
-  dark: "dark",
-};
 
 export const themeSwitcher = () => {
-  switcher.addEventListener("click", () => {
-    let themeState = {
-      previous: indicator.dataset.theme,
-      next: indicator.dataset.next,
-    };
-    switcher.id = themeState.next;
-    indicator.dataset.theme = themeState.next;
-    indicator.dataset.next = themeState.previous;
-    indicator.src = indicator.src.replace(themeState.previous, themeState.next);
-    document.body.dataset.theme = themeState.next;
-    store(themeState.next);
-  });
+  switcher.addEventListener("click", _switch);
 };
+
+const _switch = () => {
+  let themeState = {
+    previous: indicator.dataset.theme,
+    next: indicator.dataset.next,
+  };
+  switcher.id = themeState.next;
+  indicator.dataset.theme = themeState.next;
+  indicator.dataset.next = themeState.previous;
+  indicator.src = indicator.src.replace(themeState.previous, themeState.next);
+  document.body.dataset.theme = themeState.next;
+  store(themeState.next);
+}
 
 const store = (theme) => {
   document.cookie = "theme=" + theme + ";path=/";
