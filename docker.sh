743972e4a826043ec23d87e6ae8d7981d0d1fe7a
AlexisHenry <alexis.henry150357@gmail.com>
2022-11-28 12:06:29 +0100

    docker-compose

--- docker.sh
+++ docker.sh
@@ -17,21 +17,21 @@ docker_compose () {
         container=$(docker ps | grep mysql | awk '{print $1}');
 
         # Copy dump file inside the container
-		for file in $sql_files; do
-			fileName=$(basename $file);
-			docker cp $file $container:/$fileName;
-			echo "File $fileName copied to container $container";
-		done
+	for file in $sql_files; do
+		fileName=$(basename $file);
+		docker cp $file $container:/$fileName;
+		echo "File $fileName copied to container $container";
+	done
 
         # Drop all tables and re-run all migrations
         ./vendor/bin/sail artisan migrate:fresh;
 
         # Import dump file
-		for file in $sql_files; do
-			fileName=$(basename $file);
-			echo "source $fileName" | ./vendor/bin/sail mysql;
-			echo "File $fileName imported to database";
-		done
+	for file in $sql_files; do
+		fileName=$(basename $file);
+		echo "source $fileName" | ./vendor/bin/sail mysql;
+		echo "File $fileName imported to database";
+	done
 
         sudo chown -R "$USER":www-data storage/ bootstrap/ public/
 
