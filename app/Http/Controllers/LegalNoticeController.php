3279360b163bcdbbcb74e7043ba89447411db21c
AlexisHenry <alexis.henry150357@gmail.com>
2022-12-17 22:08:29 +0100

    feat: legal notice

--- app/Http/Controllers/LegalNoticeController.php
+++ app/Http/Controllers/LegalNoticeController.php
@@ -2,22 +2,18 @@
 
 namespace App\Http\Controllers;
 
-use App\Helpers\Rand;
-use App\Helpers\Skills;
-use App\Random\Random;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
-use Stichoza\GoogleTranslate\GoogleTranslate;
 use Illuminate\Routing\Controller;
 
 class LegalNoticeController extends Controller
 {
     public function index()
     {
+        $lastUpdated = now()->format('F d, Y');
         return view('pages.legals', [
             'title' => 'Portfolio - HENRY ALEXIS',
             'navbar' => '',
-            'og_description' => 'Portfolio - HENRY ALEXIS - Homepage'
+            'og_description' => 'Portfolio - HENRY ALEXIS - Homepage',
+            'lastUpdated' => $lastUpdated,
         ]);
     }
 }
