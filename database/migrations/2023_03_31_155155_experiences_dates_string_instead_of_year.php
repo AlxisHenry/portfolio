c6b8d83ff5126d4d1f7dad686cd9be14ad513243
AlexisHenry <alexis.henry150357@gmail.com>
2023-07-10 11:47:22 +0200

    hotfix: ended_at column nullable

--- database/migrations/2023_03_31_155155_experiences_dates_string_instead_of_year.php
+++ database/migrations/2023_03_31_155155_experiences_dates_string_instead_of_year.php
@@ -4,18 +4,19 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
         Schema::table('experiences', function (Blueprint $table) {
-            $table->string('started_at')
-                ->change();
-            $table->string('ended_at')
-                ->change();
+            $table->string('started_at')->change();
+            $table
+                ->string('ended_at')
+                ->change()
+                ->default(null)
+                ->nullable();
         });
     }
 
